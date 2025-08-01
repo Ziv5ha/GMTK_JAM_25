dd5c298c61a232f024b172f5a2bf9e8827462107
Ziv <zivshaul3@gmail.com>
2025-08-01 18:49:53 +0300

    We have a game! With absolutely NO graphics!

---
+++ Assets/Scripts/Hoop.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoop: MonoBehaviour {
+    public MainFlow MainFlowRef;
+    public Rigidbody2D Rigidbody2DRef;
+
+    private float _timeToStartGravityOnEdgeTouch = .3f;
+    private float _gracePeriodTimeToStartGravityOnEdgeTouch = .5f;
+    private float _timeToStartGravityAfterPerfectSpin = .1f;
+    private int _spinForce = 100;
+    private int _upforce = 10;
+    private bool _hoopIsOnTheRight = true;
+    private bool _enableHoopSpin = true;
+    private float _lastHoopEdgeTouchTime;
+    public int PerfectStreak = 0;
+    private float _perfectSpinTimeWindow = .2f;
+    private float _perfectSpinTimeWindowNarrower = .01f;
+    private int _perfectStreakSpinForceMultiplier = 10;
+    private float _perfectStreakUpForceMultiplier = .1f;
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (!MainFlowRef.GameRunning) return;
+
+        if (other.gameObject.name == "Floor") {
+            MainFlowRef.HandleGameOver();
+        }
+        if (other.gameObject.name == "Player") {
+            _enableHoopSpin = true;
+            _lastHoopEdgeTouchTime = Time.time;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other) {
+        if (!MainFlowRef.GameRunning) return;
+        _enableHoopSpin = false;
+        float totalPerfectSpinTimeWindow = _perfectSpinTimeWindow - (_perfectSpinTimeWindowNarrower * PerfectStreak);
+        if (Time.time - _lastHoopEdgeTouchTime < totalPerfectSpinTimeWindow) {
+            PerfectStreak++;
+        } else {
+            PerfectStreak = 0;
+        }
+        Debug.Log($"!@# PerfectStreak: {PerfectStreak}");
+    }
+
+    public void SpinHoop(bool right) {
+        // print($"{(right ? "right" : "left")} arrow key is held down");
+        float totalSpinForce = _spinForce + (_perfectStreakSpinForceMultiplier * PerfectStreak);
+        float totalUpForce = (_upforce + (_perfectStreakUpForceMultiplier * PerfectStreak)) * (transform.position.y > 1 ? 0 : 1);
+
+        if (!_enableHoopSpin) return;
+        if (!right && _hoopIsOnTheRight) {
+            _hoopIsOnTheRight = false;
+            Rigidbody2DRef.AddForce(new Vector2(-totalSpinForce, totalUpForce));
+        }
+        if (right && !_hoopIsOnTheRight) {
+            _hoopIsOnTheRight = true;
+            Rigidbody2DRef.AddForce(new Vector2(totalSpinForce, totalUpForce));
+        }
+    }
+    public void HandleHoopGravity(bool gracePeriod) {
+        if (!MainFlowRef.GameRunning) return;
+        float totalHoopHangTime = gracePeriod ? (_gracePeriodTimeToStartGravityOnEdgeTouch + (_timeToStartGravityAfterPerfectSpin * PerfectStreak)) : (_timeToStartGravityOnEdgeTouch + (_timeToStartGravityAfterPerfectSpin * PerfectStreak));
+        if (Time.time - _lastHoopEdgeTouchTime < totalHoopHangTime) {
+            Rigidbody2DRef.gravityScale = 0;
+        } else {
+            Rigidbody2DRef.gravityScale = 0.1f;
+        }
+    }
+}
---
+++ Assets/Scripts/MainFlow.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainFlow: MonoBehaviour {
+    public Hoop HoopRef;
+    public Player PlayerRef;
+    public bool GameRunning;
+    public int Score = 0;
+    private float _startTime;
+    private float _gracePeriod = 1f;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        GameRunning = true;
+        HoopRef.SpinHoop(false);
+        _startTime = Time.time;
+        Score = 0;
+        HoopRef.PerfectStreak = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!GameRunning) return;
+        if (Input.GetKey("left")) {
+            HoopRef.SpinHoop(false);
+        }
+
+        if (Input.GetKey("right")) {
+            HoopRef.SpinHoop(true);
+        }
+        HoopRef.HandleHoopGravity(Time.time - _startTime < _gracePeriod);
+    }
+
+    public void HandleGameOver() {
+        GameRunning = false;
+    }
+
+    public void HandleScore() {
+
+    }
+}
